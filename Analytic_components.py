made some corrections to make the layout optimization work. Also fixed adjustCtCp gradients

Analytic_components.py
@@ -52,9 +52,8 @@ class floris_adjustCtCp(Component):
         if not CTcorrected:
 
             self.Ct_out = Ct*np.cos(yaw)*np.cos(yaw)
-            #
-            dCt_dCt = np.eye(nTurbines, nTurbines)*np.cos(yaw)*np.cos(yaw)
-            dCt_dyaw = np.eye(nTurbines, nTurbines)*(-2*np.sin(yaw)*np.cos(yaw))
+            dCt_dCt = np.eye(nTurbines)*np.cos(yaw)*np.cos(yaw)
+            dCt_dyaw = np.eye(nTurbines)*(-2*Ct*np.sin(yaw)*np.cos(yaw))*np.pi/180.
             dCt_dCp = np.zeros((nTurbines, nTurbines))
             dCt = np.hstack((dCt_dCt, dCt_dCp, dCt_dyaw))
 
@@ -71,7 +70,7 @@ class floris_adjustCtCp(Component):
             self.Cp_out = Cp*np.cos(yaw)**pP
 
             dCp_dCp = np.eye(nTurbines, nTurbines)*np.cos(yaw)**pP
-            dCp_dyaw = np.eye(nTurbines, nTurbines)*(-Cp*pP*np.sin(yaw)*np.cos(yaw)**(pP-1.0))
+            dCp_dyaw = np.eye(nTurbines, nTurbines)*(-Cp*pP*np.sin(yaw)*np.cos(yaw)**(pP-1.0))*np.pi/180.
             dCp_dCt = np.zeros((nTurbines, nTurbines))
             dCp = np.hstack((dCp_dCt, dCp_dCp, dCp_dyaw))
 
@@ -314,8 +313,6 @@ class floris_AEP(Component):
 
     def __init__(self, nDirections):
 
-        # print 'entering AEP __init__ - analytic'
-
         super(floris_AEP, self).__init__()
 
         self.add('power_directions', Array(np.zeros(nDirections), iotype='in', units='kW', desc='vector containing \
@@ -323,50 +320,45 @@ class floris_AEP(Component):
         self.add('windrose_frequencies', Array(np.zeros(nDirections), iotype='in', desc='vector containing \
                                                the weighted frequency of wind at each direction ccw from east using \
                                                direction too'))
+        # do not use these for any gradient calculations, only for output
+        self.add('power_directions_out', Array(np.zeros(nDirections), iotype='out', units='kW', desc='vector containing \
+                                           the power production at each wind direction ccw from north', deriv_ignore=True))
+
 
     def execute(self):
 
-        # print 'entering AEP - analytic'
-
+        # locally name input values
         power_directions = self.power_directions
-        # print 'power_directions assigned'
         windrose_frequencies = self.windrose_frequencies
-        # print 'windrose_frequencies assigned'
-        # print power_directions, windrose_frequencies
 
         # number of hours in a year
         hours = 8760.0
 
+        # calculate approximate AEP
         AEP = sum(power_directions*windrose_frequencies)*hours
-        # print 'AEP calculated'
 
+        # promote AEP result to class attribute
         self.AEP = AEP
-
-        # print 'In AEP, AEP = %s, power_directions = %s' % (self.AEP, self.power_directions)
-        # print 'AEP passed to component'
+        self.power_directions_out = power_directions
 
     def list_deriv_vars(self):
 
-        # print 'entered list_deriv_vars()'
-
+        # return ('power_directions',), ('AEP', 'power_directions_out')
         return ('power_directions',), ('AEP',)
 
     def provideJ(self):
 
-        print 'entering provideJ()'
-
+        # create local variables
         windrose_frequencies = self.windrose_frequencies
         ndirs = np.size(windrose_frequencies)
 
         # number of hours in a year
         hours = 8760.0
 
+        # calculate the derivative of outputs w.r.t. each wind direction
         dAEP_dpower = np.ones(ndirs)*windrose_frequencies*hours
 
         J = np.array([dAEP_dpower])
-        # print 'J = ', J
-
-        # print 'J assigned'
 
         return J
 

Tapenade_components.py
@@ -375,7 +375,7 @@ class floris_power(Component):
 
         # construct Jacobian of power
         dpower = np.hstack((wakeOverlapTRel_vb, Ctb, axialInductionb, turbineXwb, rotorDiameterb, Cpb))
-        dpower = dpower[0, :]
+        dpower = dpower[0., :]
 
         # construct total Jacobian of floris_power
         J = np.vstack((dvelocitiesTurbines, dwt_power, dpower))

test_call.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
 
 
     rotor_diameter = 126.4
-    nRows = 10.
+    nRows = 3.
     spacing = 7.
 
     # windrose for test case from Pieter
@@ -31,7 +31,8 @@ if __name__ == "__main__":
                0.0138723040032756,0.0134869622254069,0.0131224497328283,0.0127771221082802,0.0124495035926833,0.0121382660028662,
                0.0118422107345036,0.011560253336063,0.0112914102352243,0.0110347872753329,0.0107895697803255,0.0105550139155358])
 
-    # dirPercent = np.array([.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1])
+    # dirPercent = np.array([.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.4])
+    # dirPercent = np.array([1.0])
     nDirections = len(dirPercent)
 
     points = np.arange(start=spacing*rotor_diameter, stop=nRows*spacing*rotor_diameter+1, step=spacing*rotor_diameter)
@@ -80,8 +81,8 @@ if __name__ == "__main__":
         Ct[turbI] = 4.0*axialInduction[turbI]*(1.0-axialInduction[turbI])
         Cp[turbI] = 0.7737/0.944 * 4.0 * 1.0/3.0 * np.power((1 - 1.0/3.0), 2.)
         generator_efficiency[turbI] = 0.944
+        # yaw[turbI] = 25.
         yaw[turbI] = 0.
-        # yaw[turbI] = 0
 
     # yaw[0] = 30
 
@@ -108,12 +109,12 @@ if __name__ == "__main__":
     # myFloris.wind_direction = 30.  # deg ccw from east using direction too
     myFloris.verbose = False
 
-    myFloris.windrose_directions = 270 - np.arange(0.0, 360.0, 360.0/nDirections)
+    myFloris.windrose_directions = 270. - np.arange(0.0, 360.0, 360.0/nDirections)
     # myFloris.windrose_directions = 270 - windDirs
     print myFloris.windrose_directions
     for i in range(0, nDirections):
-        if myFloris.windrose_directions[i] < 0:
-            myFloris.windrose_directions[i] += 360
+        if myFloris.windrose_directions[i] < 0.:
+            myFloris.windrose_directions[i] += 360.
 
     myFloris.parameters.CPcorrected = False
     myFloris.parameters.CTcorrected = False
@@ -135,30 +136,25 @@ if __name__ == "__main__":
     print('FLORIS Opt. calculation took %.03f sec.' % (toc-tic))
 
     # Display returns
-    # print 'turbine powers (kW): %s' % myFloris.wt_power
-    # print 'turbine powers (kW): %s' % myFloris2.wt_power
-    print 'turbine powers (kW): %s' % myFloris.wt_power_directions
+
+    # TODO get this for loop to work so you can print the desired values, or figure out another way to do this
+    # for i in range(0, nDirections):
+    #     print 'turbine powers for %d deg. (kW): %s' % (myFloris.windrose_directions[i],
+    #                                                    myFloris.floris_power_(i).wt_power_directions)
+    #     print 'effective wind speeds for %d deg. (m/s): %s' % (myFloris.windrose_directions[i],
+    #                                                            myFloris.floris_power_[i].velocitiesTurbines)
+    #
+    #     print 'wind farm power for %d deg. (kW): %s' % (myFloris.windrose_directions[i],
+    #                                                     myFloris.floris_power_[i].power_directions)
+    #     print 'wind turbine powers for %d deg. (kW): %s' % (myFloris.windrose_directions[i],
+    #                                                         myFloris.floris_power_[i].wt_power)
+
+    print 'power in each wind direction (kW): %s' % myFloris.power_directions
     print 'turbine X positions in wind frame (m): %s' % myFloris.turbineX
     print 'turbine Y positions in wind frame (m): %s' % myFloris.turbineY
     print 'yaw (deg) = ', myFloris.yaw
-    # print 'yaw (deg) = ', myFloris.floris_adjustCtCp.yaw
-    # print 'yaw (deg) = ', myFloris.floris_adjustCtCp.yaw
-    # print 'effective wind speeds (m/s): %s' % myFloris.velocitiesTurbines
-    print 'effective wind speeds (m/s): %s' % myFloris.floris_power_0.velocitiesTurbines
-    # print 'Wake center Y positions (m): %s' % myFloris2.wakeCentersYT
-    # print 'Wake diameters (m): %s' % myFloris2.wakeDiametersT
-    # print 'Relative wake overlap (m*m): %s' % myFloris2.wakeOverlapTRel
-    # print 'wind farm power (kW): %s' % myFloris.power
-    print 'wind farm power (kW): %s' % myFloris.power_directions
     print 'AEP (kWh): %s' % myFloris.AEP
 
-    # windDirection = myFloris.wind_direction
-    # rotationMatrix = np.array([(np.cos(windDirection), -np.sin(windDirection)),
-    #                                (np.sin(windDirection), np.cos(windDirection))])
-    # # print 'rotation matrix = ', rotationMatrix
-    # turbinepos_opt = np.dot(rotationMatrix, np.array([myFloris.turbineXw, myFloris.turbineYw]))
-    # print turbinepos_opt
-    #
     resolution = 200
     myFloris2 = floris_assembly(nTurbines=nTurbs, resolution=resolution)
     myFloris2.turbineX = myFloris.turbineX

test_case_assembly.py
@@ -51,9 +51,6 @@ class floris_assembly_opt_AEP(Assembly):
         self.nTurbines = nTurbines
         self.resolution = resolution
         self.nDirections = nDirections
-        # directions = 270 - np.arange(0.0, 360.0, 360.0/nDirections)
-        # print directions
-        # Explicitly size input arrays
 
         # wt_layout input variables
         self.add('rotorDiameter', Array(np.zeros(nTurbines), dtype='float', iotype='in', units='m',
@@ -104,27 +101,27 @@ class floris_assembly_opt_AEP(Assembly):
         self.add('power_directions', Array(np.zeros(nDirections), iotype='out', units='kW', desc='total windfarm power \
                                            in each direction cw from north using direction from'))
 
-        # print 'end of init'
     def configure(self):
 
         # add driver so the workflow is not overwritten later
         # self.add('driver', SLSQPdriver())
         # self.driver.gradient_options.force_fd = True
-        self.add('driver', pyOptDriver())
-        self.driver.optimizer = 'SNOPT'
+        # self.add('driver', pyOptDriver())
+        # self.driver.optimizer = 'SNOPT'
         # self.driver.pyopt_diff = True
 
         # add AEP component first so it can be connected to
-        self.add('floris_AEP', floris_AEP(nDirections=self.nDirections))
+        F6 = self.add('floris_AEP', floris_AEP(nDirections=self.nDirections))
+        F6.missing_deriv_policy = 'assume_zero'
         self.connect('windrose_frequencies', 'floris_AEP.windrose_frequencies')
         self.connect('floris_AEP.AEP', 'AEP')
+        self.connect('floris_AEP.power_directions_out', 'power_directions')
 
         print 'in configure, diirections = ', self.windrose_directions
 
         for i in range(0, self.nDirections):
 
             print 'i = %s' % i
-            # print self.nDirections
 
             # add components to floris assembly
             F1 = self.add('floris_adjustCtCp_%d' % i, floris_adjustCtCp(nTurbines=self.nTurbines))
@@ -184,29 +181,29 @@ class floris_assembly_opt_AEP(Assembly):
 
             # connections from floris_overlap to floris_power
             self.connect('floris_overlap_%d.wakeOverlapTRel' % i, 'floris_power_%d.wakeOverlapTRel' % i)
+
+            # connections from floris_power to floris_AEP
+            self.connect('floris_power_%d.power' % i, 'floris_AEP.power_directions[%d]' % i)
             # #################################################################
 
             # output connections
-            # self.connect('floris_power_%d.velocitiesTurbines[:]' % i, 'velocitiesTurbines_directions[%d, :]' % i)
-            # self.connect('floris_power_%d.wt_power[:]' % i, 'wt_power_directions[%d, :]' % i)
-            self.connect('floris_power_%d.power' % i, 'floris_AEP.power_directions[%d]' % i)
+            # self.connect('floris_power_%d.velocitiesTurbines' % i, 'velocitiesTurbines_directions[%d]' % i)
+            # self.connect('floris_power_%d.wt_power' % i, 'wt_power_directions[%d]' % i)
+            # self.connect('floris_power_%d.power' % i, 'power_directions[%d]' % i)
 
             # add to workflow
             self.driver.workflow.add(['floris_adjustCtCp_%d' % i, 'floris_windframe_%d' % i,
                                       'floris_wcent_wdiam_%d' % i, 'floris_overlap_%d' % i, 'floris_power_%d' % i])
 
-        # self.connect('floris_AEP.power_directions', 'power_directions')
-        print 'finished loop'
         # add AEP calculations to workflow
         self.driver.workflow.add(['floris_AEP'])
-        print 'workflow added'
         # set up driver
-        self.driver.iprint = 3
-        self.driver.accuracy = 1.0e-12
-        self.driver.maxiter = 100
-        self.driver.add_objective('-floris_AEP.AEP')
-        self.driver.add_parameter('turbineX', low=7*126.4, high=np.sqrt(self.nTurbines)*7*126.4)
-        self.driver.add_parameter('turbineY', low=7*126.4, high=np.sqrt(self.nTurbines)*7*126.4)
+        # self.driver.iprint = 3
+        # self.driver.accuracy = 1.0e-12
+        # self.driver.maxiter = 100
+        # self.driver.add_objective('-floris_AEP.AEP')
+        # self.driver.add_parameter('turbineX', low=7*126.4, high=np.sqrt(self.nTurbines)*7*126.4)
+        # self.driver.add_parameter('turbineY', low=7*126.4, high=np.sqrt(self.nTurbines)*7*126.4)
         # self.driver.add_parameter('yaw', low=-30., high=30., scaler=1)
 
 